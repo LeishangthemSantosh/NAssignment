2622fecd1a0f4ec0fffbe7b51e5d0100edc9a9ec
Leishangthem Santosh <santoshleishangthem1997@gmail.com>
2025-03-30 02:56:16 +0530

    Optimize

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,30 +1,34 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import { salesDataSeed } from '../utils/db.seed.js';
 
-const DB_FILE = '../../database.sqlite';
+const DB_FILE = './database.sqlite';
+
+export const dbPromise = open({
+    filename: DB_FILE,
+    driver: sqlite3.Database
+});
 
 // Initialize SQLite
 export const initDB = async () => {
-    const db = await open({
-        filename: DB_FILE,
-        driver: sqlite3.Database
-    });
+    
+    const db = await dbPromise;
     
     // Check if table exist
-    const tables = await db.all("SELECT name FROM sqlite_master WHERE type='table' AND name IN ('sales_data', 'users');");
+    const tables = await db.all("SELECT name FROM sqlite_master WHERE type='table' AND name IN ('sales', 'users');");
     const tableNames = tables.map(t => t.name);
     
-    if (!tableNames.includes('sales_data')) {
+    if (!tableNames.includes('sales')) {
         await db.exec(`
-            CREATE TABLE sales_data (id INTEGER PRIMARY KEY, date TEXT, amount INTEGER);
-            INSERT INTO sales_data (date, amount) VALUES ('2024-01-01', 1000), ('2024-02-01', 2000);
+            CREATE TABLE sales (id INTEGER PRIMARY KEY, date TEXT, amount INTEGER);
         `);
+        await salesDataSeed(db)
     }
     
     if (!tableNames.includes('users')) {
         await db.exec(`
             CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT, active BOOLEAN);
-            INSERT INTO users (name, email, active) VALUES ('Alice', 'naocha@gmail.com', 1), ('Bob', 'tomba@gmail.com', 0);
+            INSERT INTO users (name, email, active) VALUES ('Admin', 'naocha@gmail.com', 1), ('Bob', 'bob@gmail.com', 0);
         `);
     }
     return db;
